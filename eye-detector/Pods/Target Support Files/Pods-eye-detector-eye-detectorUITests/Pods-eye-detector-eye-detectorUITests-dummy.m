73922cb8f091d6501b3b97c1cab31a7a05cf0d3b
Brandon Hancock <brandon.e.hancock@outlook.com>
2020-08-30 18:41:50 -0700

    Adding tensorflow lite to project

---
+++ eye-detector/Pods/Target Support Files/Pods-eye-detector-eye-detectorUITests/Pods-eye-detector-eye-detectorUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_eye_detector_eye_detectorUITests : NSObject
+@end
+@implementation PodsDummy_Pods_eye_detector_eye_detectorUITests
+@end
---
+++ eye-detector/Pods/Target Support Files/Pods-eye-detector/Pods-eye-detector-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_eye_detector : NSObject
+@end
+@implementation PodsDummy_Pods_eye_detector
+@end
