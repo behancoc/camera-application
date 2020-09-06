3135840c4c42ffc833a19bb0edb578f211d93e09
Brandon Hancock <brandon.e.hancock@outlook.com>
2020-09-05 17:58:41 -0700

    Updates to UI with storyboard

--- EyeDetectionApplication/EyeDetectionApplication/ViewController.swift
+++ EyeDetectionApplication/EyeDetectionApplication/ViewController.swift
@@ -10,11 +10,29 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var cameraButton: UIBarButtonItem!
+    @IBOutlet weak var searchButton: UIBarButtonItem!
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var detectEyesButton: UIButton!
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func cameraButtonPressed(_ sender: Any) {
+        print("Camera Button Pressed")
+    }
+    
+    @IBAction func searchButtonPressed(_ sender: Any) {
+        print("Search Button Pressed")
+    }
+    
+    @IBAction func detectEyesButtonPressed(_ sender: Any) {
+        print("Detect Eyes Button Pressed")
+    }
+    
 }
 
