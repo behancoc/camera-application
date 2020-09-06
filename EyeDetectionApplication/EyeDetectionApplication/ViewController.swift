9fd7939f39ac37c18ec27c11000e380e91f60f28
Brandon Hancock <brandon.e.hancock@outlook.com>
2020-09-05 17:12:31 -0700

    Start of new application using Storyboard instead of SwiftUI

---
+++ EyeDetectionApplication/EyeDetectionApplication/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  EyeDetectionApplication
+//
+//  Created by Brandon Hancock on 9/5/20.
+//  Copyright © 2020 Brandon Hancock. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
